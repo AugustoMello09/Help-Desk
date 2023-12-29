20a4b104c3efe7e6f8b8ea9f9590cfdffba26f10
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-12-29 16:17:06 -0300

    projeto criado

---
+++ helpDesk-Backend/src/main/java/io/gitHub/AugustoMello09/helpDesk/HelpDeskApplication.java
@@ -0,0 +1,13 @@
+package io.gitHub.AugustoMello09.helpDesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpDeskApplication.class, args);
+	}
+
+}
---
+++ helpDesk-Backend/src/test/java/io/gitHub/AugustoMello09/helpDesk/HelpDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gitHub.AugustoMello09.helpDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
