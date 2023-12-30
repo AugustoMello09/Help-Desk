2f398c9a57e01eb0e4b64281c6740453f70f8eaa
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2023-12-30 10:43:33 -0300

    feat: Usuario e Cargo relacionados e modelados

---
+++ helpDesk-Backend/src/main/java/io/gitHub/AugustoMello09/helpDesk/entities/Cargo.java
@@ -0,0 +1,31 @@
+package io.gitHub.AugustoMello09.helpDesk.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_cargo")
+public class Cargo  implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String authority;
+	
+	public Cargo() {}
+
+}
---
+++ helpDesk-Backend/src/main/java/io/gitHub/AugustoMello09/helpDesk/entities/Usuario.java
@@ -0,0 +1,46 @@
+package io.gitHub.AugustoMello09.helpDesk.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_usuario")
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Usuario implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+	
+	private String nome;
+	private String email;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_usuario_cargo", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "cargo_id"))
+	private Set<Cargo> cargos = new HashSet<>();
+	
+	public Usuario() {}
+
+}
