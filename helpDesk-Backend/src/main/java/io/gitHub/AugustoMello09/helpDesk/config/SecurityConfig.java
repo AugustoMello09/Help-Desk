d37104c65402b6c6d17d281e6872312c556acc8c
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2024-02-20 18:21:58 -0300

    feat: liberando h2 e swagger

--- helpDesk-Backend/src/main/java/io/gitHub/AugustoMello09/helpDesk/config/SecurityConfig.java
+++ helpDesk-Backend/src/main/java/io/gitHub/AugustoMello09/helpDesk/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package io.gitHub.AugustoMello09.helpDesk.config;
 
+import java.util.Arrays;
+
+import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,29 +21,38 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
-	
+
 	private static final String[] PUBLIC_POST = { "/auth/login", "/cliente", "/tecnico" };
-	
+
+	private static final String[] PUBLIC_GET = { "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**",
+			"/h2-console/**" };
+
 	@Autowired
 	private SecurityFilter securityFilter;
-	
+
+	@Autowired
+	private Environment env;
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		return http.csrf(csrf -> csrf.disable())
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers((headers) -> headers.frameOptions((frameOptions) -> frameOptions.disable()));
+		}
+		http.csrf(csrf -> csrf.disable())
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(authorize -> authorize
-				.requestMatchers(HttpMethod.POST, PUBLIC_POST).permitAll()
-				.anyRequest().authenticated()
-				)
-				.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-				.build();
+				.authorizeHttpRequests(authorize -> authorize.requestMatchers(HttpMethod.POST, PUBLIC_POST).permitAll()
+						.requestMatchers(PUBLIC_GET).permitAll().anyRequest().authenticated())
+				.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+		return http.build();
+
 	}
-	
+
 	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+			throws Exception {
 		return authenticationConfiguration.getAuthenticationManager();
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
